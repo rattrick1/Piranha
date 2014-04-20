a5893a2329627251075648250998cdfa6d643352
Scott Scowden <rattrick1@gmail.com>
2014-04-20 15:52:44 -0400

    Added config option to allow EF to use a custom schema
    We know DBAs can be picky. I created an easy way to run Piranha under an
    alternate schema.

--- Core/Piranha/Config.cs
+++ Core/Piranha/Config.cs
@@ -7,176 +7,211 @@ using System.Web;
 
 namespace Piranha
 {
-	/// <summary>
-	/// Provides access to web.config parameters.
-	/// </summary>
-	public static class Config
-	{
-		#region Members
-		private static string[] namespaces = null;
-		private static object nsmutex = new object() ;
-		private static readonly ConfigFile config = GetConfig() ;
-		private static bool? prefixlessPermalinks = null ;
-		#endregion
+    /// <summary>
+    /// Provides access to web.config parameters.
+    /// </summary>
+    public static class Config
+    {
+        #region Members
+        private static string[] namespaces = null;
+        private static object nsmutex = new object();
+        private static readonly ConfigFile config = GetConfig();
+        private static bool? prefixlessPermalinks = null;
+        #endregion
 
-		/// <summary>
-		/// Gets if method binding is disabled. When method binding is enabled, the first UrlData
-		/// argument on a GET will be matched against the available methods in the executing page class.
-		/// If a matching method is found this method will be executed instead of the usual ExecutePage.
-		/// </summary>
-		public static bool DisableMethodBinding {
-			get { 
-				return config.Settings.DisableMethodBinding.Value ;
-			}
-		}
+        /// <summary>
+        /// Gets if method binding is disabled. When method binding is enabled, the first UrlData
+        /// argument on a GET will be matched against the available methods in the executing page class.
+        /// If a matching method is found this method will be executed instead of the usual ExecutePage.
+        /// </summary>
+        public static bool DisableMethodBinding
+        {
+            get
+            {
+                return config.Settings.DisableMethodBinding.Value;
+            }
+        }
 
-		/// <summary>
-		/// Gets if model state binding is disabled. When model state binding is enabled, values
-		/// mapped by the ModelBinder is automatically stored into the model state of the current page.
-		/// </summary>
-		public static bool DisableModelStateBinding {
-			get { 
-				return config.Settings.DisableModelStateBinding.Value ;
-			}
-		}
+        /// <summary>
+        /// Gets if model state binding is disabled. When model state binding is enabled, values
+        /// mapped by the ModelBinder is automatically stored into the model state of the current page.
+        /// </summary>
+        public static bool DisableModelStateBinding
+        {
+            get
+            {
+                return config.Settings.DisableModelStateBinding.Value;
+            }
+        }
 
-		/// <summary>
-		/// Gets if the manager interface should be disabled for the current instance.
-		/// </summary>
-		public static bool DisableManager {
-			get {
-				return config.Settings.DisableManager.Value ;
-			}
-		}
+        /// <summary>
+        /// Gets if the manager interface should be disabled for the current instance.
+        /// </summary>
+        public static bool DisableManager
+        {
+            get
+            {
+                return config.Settings.DisableManager.Value;
+            }
+        }
 
-		/// <summary>
-		/// Gets the additional namespaces that should be inlcuded in the manager interface.
-		/// </summary>
-		public static string[] ManagerNamespaces {
-			get {
-				if (namespaces != null)
-					return namespaces ;
-				
-				lock (nsmutex) {
-					if (namespaces == null) {
-						var str = config.Settings.ManagerNamespaces.Value ;
+        /// <summary>
+        /// Gets an optional default schema for Entity Framework.
+        /// </summary>
+        public static string EntitySchema
+        {
+            get
+            {
+                return config.Settings.EntitySchema.Value;
+            }
+        }
 
-						if (!String.IsNullOrEmpty(str)) {
-							var tmp = str.Split(new char[] {','}) ;
+        /// <summary>
+        /// Gets the additional namespaces that should be inlcuded in the manager interface.
+        /// </summary>
+        public static string[] ManagerNamespaces
+        {
+            get
+            {
+                if (namespaces != null)
+                    return namespaces;
 
-							for (int n = 0; n < tmp.Length; n++)
-								tmp[n] = tmp[n].Trim() ;
-							namespaces = tmp ;
-						} else {
-							namespaces = new string[0] ;
-						}
-					}
-				}
-				return namespaces ;
-			}
-		}
+                lock (nsmutex)
+                {
+                    if (namespaces == null)
+                    {
+                        var str = config.Settings.ManagerNamespaces.Value;
 
-		/// <summary>
-		/// Gets if the application is running in passive mode or not.
-		/// </summary>
-		public static bool PassiveMode {
-			get {
-				return config.Settings.PassiveMode.Value ;
-			}
-		}
+                        if (!String.IsNullOrEmpty(str))
+                        {
+                            var tmp = str.Split(new char[] { ',' });
 
-		/// <summary>
-		/// Gets/sets whether to use prefixless permalinks or not.
-		/// </summary>
-		public static bool PrefixlessPermalinks {
-			get {
-				if (!prefixlessPermalinks.HasValue)
-					return config.Settings.PrefixlessPermalinks.Value ;
-				return prefixlessPermalinks.Value ;
-			}
-			set {
-				prefixlessPermalinks = value ;
-			}
-		}
+                            for (int n = 0; n < tmp.Length; n++)
+                                tmp[n] = tmp[n].Trim();
+                            namespaces = tmp;
+                        }
+                        else
+                        {
+                            namespaces = new string[0];
+                        }
+                    }
+                }
+                return namespaces;
+            }
+        }
 
-		/// <summary>
-		/// Gets the optional internal id of the current sitetree. If the parameter is empty
-		/// the current host name is resolved. If the hostname isn't found DEFAULT_SITE is used.
-		/// </summary>
-		public static string SiteTree {
-			get { return WebPages.WebPiranha.CurrentSite.InternalId ; }
-		}
+        /// <summary>
+        /// Gets if the application is running in passive mode or not.
+        /// </summary>
+        public static bool PassiveMode
+        {
+            get
+            {
+                return config.Settings.PassiveMode.Value;
+            }
+        }
 
-		/// <summary>
-		/// Gets the id of the currently active site tree.
-		/// </summary>
-		public static Guid SiteTreeId {
-			get { return WebPages.WebPiranha.CurrentSite.Id ; }
-		}
+        /// <summary>
+        /// Gets/sets whether to use prefixless permalinks or not.
+        /// </summary>
+        public static bool PrefixlessPermalinks
+        {
+            get
+            {
+                if (!prefixlessPermalinks.HasValue)
+                    return config.Settings.PrefixlessPermalinks.Value;
+                return prefixlessPermalinks.Value;
+            }
+            set
+            {
+                prefixlessPermalinks = value;
+            }
+        }
 
-		/// <summary>
-		/// Gets the id of the namespace for the currently active site tree.
-		/// </summary>
-		public static Guid SiteTreeNamespaceId {
-			get { return WebPages.WebPiranha.CurrentSite.NamespaceId ; }
-		}
+        /// <summary>
+        /// Gets the optional internal id of the current sitetree. If the parameter is empty
+        /// the current host name is resolved. If the hostname isn't found DEFAULT_SITE is used.
+        /// </summary>
+        public static string SiteTree
+        {
+            get { return WebPages.WebPiranha.CurrentSite.InternalId; }
+        }
 
-		/// <summary>
-		/// Gets the id of the default sys user.
-		/// </summary>
-		public static readonly Guid SysUserId = new Guid("CA19D4E7-92F0-42F6-926A-68413BBDAFBC") ;
+        /// <summary>
+        /// Gets the id of the currently active site tree.
+        /// </summary>
+        public static Guid SiteTreeId
+        {
+            get { return WebPages.WebPiranha.CurrentSite.Id; }
+        }
 
-		/// <summary>
-		/// Gets the id of the default sys admin group.
-		/// </summary>
-		public static readonly Guid SysAdminGroupId = new Guid("7C536B66-D292-4369-8F37-948B32229B83") ;
+        /// <summary>
+        /// Gets the id of the namespace for the currently active site tree.
+        /// </summary>
+        public static Guid SiteTreeNamespaceId
+        {
+            get { return WebPages.WebPiranha.CurrentSite.NamespaceId; }
+        }
 
-		/// <summary>
-		/// Gets the id of the default namespace.
-		/// </summary>
-		public static readonly Guid DefaultNamespaceId = new Guid("8FF4A4B4-9B6C-4176-AAA2-DB031D75AC03") ;
+        /// <summary>
+        /// Gets the id of the default sys user.
+        /// </summary>
+        public static readonly Guid SysUserId = new Guid("CA19D4E7-92F0-42F6-926A-68413BBDAFBC");
 
-		/// <summary>
-		/// Gets the id of the archive namespace.
-		/// </summary>
-		public static readonly Guid ArchiveNamespaceId = new Guid("C8342FB4-D38E-4EAF-BBC1-4EF3BDD7500C") ;
+        /// <summary>
+        /// Gets the id of the default sys admin group.
+        /// </summary>
+        public static readonly Guid SysAdminGroupId = new Guid("7C536B66-D292-4369-8F37-948B32229B83");
 
-		/// <summary>
-		/// Gets the id of the media namespace.
-		/// </summary>
-		public static readonly Guid MediaNamespaceId = new Guid("368249B1-7F9C-4974-B9E3-A55D068DD9B6") ;
+        /// <summary>
+        /// Gets the id of the default namespace.
+        /// </summary>
+        public static readonly Guid DefaultNamespaceId = new Guid("8FF4A4B4-9B6C-4176-AAA2-DB031D75AC03");
 
-		/// <summary>
-		/// Gets the id of the default site tree.
-		/// </summary>
-		public static readonly Guid DefaultSiteTreeId = new Guid("c2f87b2b-f585-4696-8a2b-3c9df882701e");
+        /// <summary>
+        /// Gets the id of the archive namespace.
+        /// </summary>
+        public static readonly Guid ArchiveNamespaceId = new Guid("C8342FB4-D38E-4EAF-BBC1-4EF3BDD7500C");
 
-		#region Private methods
-		/// <summary>
-		/// Gets the configuration section from the Web.config.
-		/// </summary>
-		/// <returns></returns>
-		private static ConfigFile GetConfig() {
-			var section = (ConfigFile)ConfigurationManager.GetSection("piranha") ;
+        /// <summary>
+        /// Gets the id of the media namespace.
+        /// </summary>
+        public static readonly Guid MediaNamespaceId = new Guid("368249B1-7F9C-4974-B9E3-A55D068DD9B6");
 
-			if (section == null)
-				section = new ConfigFile() ;
-			return section ;
-		}
+        /// <summary>
+        /// Gets the id of the default site tree.
+        /// </summary>
+        public static readonly Guid DefaultSiteTreeId = new Guid("c2f87b2b-f585-4696-8a2b-3c9df882701e");
 
-		/// <summary>
-		/// Gets a provider object from the given string
-		/// </summary>
-		/// <param name="str">The provider string</param>
-		/// <returns>The parsed provider</returns>
-		private static ConfigProvider GetProvider(string str) {
-			var vals = str.Split(new char[] { ',' }) ;
+        #region Private methods
+        /// <summary>
+        /// Gets the configuration section from the Web.config.
+        /// </summary>
+        /// <returns></returns>
+        private static ConfigFile GetConfig()
+        {
+            var section = (ConfigFile)ConfigurationManager.GetSection("piranha");
 
-			return new ConfigProvider() {
-				TypeName = vals[0].Trim(),
-				AssemblyName = vals[1].Trim()
-			} ;
-		} 
-		#endregion
-	}
+            if (section == null)
+                section = new ConfigFile();
+            return section;
+        }
+
+        /// <summary>
+        /// Gets a provider object from the given string
+        /// </summary>
+        /// <param name="str">The provider string</param>
+        /// <returns>The parsed provider</returns>
+        private static ConfigProvider GetProvider(string str)
+        {
+            var vals = str.Split(new char[] { ',' });
+
+            return new ConfigProvider()
+            {
+                TypeName = vals[0].Trim(),
+                AssemblyName = vals[1].Trim()
+            };
+        }
+        #endregion
+    }
 }
--- Core/Piranha/ConfigFile.cs
+++ Core/Piranha/ConfigFile.cs
@@ -3,139 +3,161 @@ using System.Configuration;
 
 namespace Piranha
 {
-	/// <summary>
-	/// The main configuration section for Piranha CMS
-	/// </summary>
-	internal class ConfigFile : ConfigurationSection
-	{
-		#region Members
-		private const string SETTINGS = "settings" ;
-		private const string PROVIDERS = "providers" ;
-		#endregion
+    /// <summary>
+    /// The main configuration section for Piranha CMS
+    /// </summary>
+    internal class ConfigFile : ConfigurationSection
+    {
+        #region Members
+        private const string SETTINGS = "settings";
+        private const string PROVIDERS = "providers";
+        #endregion
 
-		/// <summary>
-		/// Gets/sets the settings.
-		/// </summary>
-		[ConfigurationProperty(SETTINGS, IsRequired=false)]
-		public SettingsElement Settings {
-			get { return (SettingsElement)this[SETTINGS] ; }
-			set { this[SETTINGS] = value ; }
-		}
+        /// <summary>
+        /// Gets/sets the settings.
+        /// </summary>
+        [ConfigurationProperty(SETTINGS, IsRequired = false)]
+        public SettingsElement Settings
+        {
+            get { return (SettingsElement)this[SETTINGS]; }
+            set { this[SETTINGS] = value; }
+        }
 
-		/// <summary>
-		/// Default constructor.
-		/// </summary>
-		public ConfigFile() {
-			Settings = new SettingsElement() ;
-		}
-	}
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public ConfigFile()
+        {
+            Settings = new SettingsElement();
+        }
+    }
 
-	/// <summary>
-	/// The settings element of the configuration section.
-	/// </summary>
-	internal class SettingsElement : ConfigurationElement
-	{
-		#region Members
-		private const string DISABLE_METHOD_BINDING = "disableMethodBinding" ;
-		private const string DISABLE_MODELSTATE_BINDING = "disableModelStateBinding" ;
-		private const string DISABLE_MANAGER = "disableManager" ;
-		private const string MANAGER_NAMESPACES = "managerNamespaces" ;
-		private const string PASSIVE_MODE = "passiveMode" ;
-		private const string PREFIXLESS_PERMALINKS = "prefixlessPermalinks" ;
-		#endregion
+    /// <summary>
+    /// The settings element of the configuration section.
+    /// </summary>
+    internal class SettingsElement : ConfigurationElement
+    {
+        #region Members
+        private const string DISABLE_METHOD_BINDING = "disableMethodBinding";
+        private const string DISABLE_MODELSTATE_BINDING = "disableModelStateBinding";
+        private const string DISABLE_MANAGER = "disableManager";
+        private const string ENTITY_SCHEMA = "entitySchema";
+        private const string MANAGER_NAMESPACES = "managerNamespaces";
+        private const string PASSIVE_MODE = "passiveMode";
+        private const string PREFIXLESS_PERMALINKS = "prefixlessPermalinks";
+        #endregion
 
-		/// <summary>
-		/// Gets/sets if method binding for Web Pages should be disabled or not.
-		/// </summary>
-		[ConfigurationProperty(DISABLE_METHOD_BINDING, IsRequired=false)]
-		public BooleanElement DisableMethodBinding {
-			get { return (BooleanElement)this[DISABLE_METHOD_BINDING] ; }
-			set { this[DISABLE_METHOD_BINDING] = value ; }
-		}
+        /// <summary>
+        /// Gets/sets if method binding for Web Pages should be disabled or not.
+        /// </summary>
+        [ConfigurationProperty(DISABLE_METHOD_BINDING, IsRequired = false)]
+        public BooleanElement DisableMethodBinding
+        {
+            get { return (BooleanElement)this[DISABLE_METHOD_BINDING]; }
+            set { this[DISABLE_METHOD_BINDING] = value; }
+        }
 
-		/// <summary>
-		/// Gets/sets if modelstate binding for Web Pages should be disabled or not.
-		/// </summary>
-		[ConfigurationProperty(DISABLE_MODELSTATE_BINDING, IsRequired=false)]
-		public BooleanElement DisableModelStateBinding {
-			get { return (BooleanElement)this[DISABLE_MODELSTATE_BINDING] ; }
-			set { this[DISABLE_MODELSTATE_BINDING] = value ; }
-		}
+        /// <summary>
+        /// Gets/sets if modelstate binding for Web Pages should be disabled or not.
+        /// </summary>
+        [ConfigurationProperty(DISABLE_MODELSTATE_BINDING, IsRequired = false)]
+        public BooleanElement DisableModelStateBinding
+        {
+            get { return (BooleanElement)this[DISABLE_MODELSTATE_BINDING]; }
+            set { this[DISABLE_MODELSTATE_BINDING] = value; }
+        }
 
-		/// <summary>
-		/// Gets/sets if the manager interface should be disabled or not.
-		/// </summary>
-		[ConfigurationProperty(DISABLE_MANAGER, IsRequired=false)]
-		public BooleanElement DisableManager {
-			get { return (BooleanElement)this[DISABLE_MANAGER] ; }
-			set { this[DISABLE_MANAGER] = value ; }
-		}
+        /// <summary>
+        /// Gets/sets if the manager interface should be disabled or not.
+        /// </summary>
+        [ConfigurationProperty(DISABLE_MANAGER, IsRequired = false)]
+        public BooleanElement DisableManager
+        {
+            get { return (BooleanElement)this[DISABLE_MANAGER]; }
+            set { this[DISABLE_MANAGER] = value; }
+        }
 
-		/// <summary>
-		/// Gets/sets the additional manager namespaces.
-		/// </summary>
-		[ConfigurationProperty(MANAGER_NAMESPACES, IsRequired=false)]
-		public StringElement ManagerNamespaces {
-			get { return (StringElement)this[MANAGER_NAMESPACES] ; }
-			set { this [MANAGER_NAMESPACES] = value ; }
-		}
+        /// <summary>
+        /// Gets/sets an optional default schema for Entity Framework.
+        /// </summary>
+        [ConfigurationProperty(ENTITY_SCHEMA, IsRequired = false)]
+        public StringElement EntitySchema
+        {
+            get { return (StringElement)this[ENTITY_SCHEMA]; }
+            set { this[ENTITY_SCHEMA] = value; }
+        }
 
-		/// <summary>
-		/// Gets/sets if the application is running in passive mode.
-		/// </summary>
-		[ConfigurationProperty(PASSIVE_MODE, IsRequired=false)]
-		public BooleanElement PassiveMode {
-			get { return (BooleanElement)this[PASSIVE_MODE] ; }
-			set { this[PASSIVE_MODE] = value ; }
-		}
+        /// <summary>
+        /// Gets/sets the additional manager namespaces.
+        /// </summary>
+        [ConfigurationProperty(MANAGER_NAMESPACES, IsRequired = false)]
+        public StringElement ManagerNamespaces
+        {
+            get { return (StringElement)this[MANAGER_NAMESPACES]; }
+            set { this[MANAGER_NAMESPACES] = value; }
+        }
 
-		/// <summary>
-		/// Gets/sets if the generated permalinks should be prefixless.
-		/// </summary>
-		[ConfigurationProperty(PREFIXLESS_PERMALINKS, IsRequired=false)]
-		public BooleanElement PrefixlessPermalinks {
-			get { return (BooleanElement)this[PREFIXLESS_PERMALINKS] ; }
-			set { this[PREFIXLESS_PERMALINKS] = value ; }
-		}
+        /// <summary>
+        /// Gets/sets if the application is running in passive mode.
+        /// </summary>
+        [ConfigurationProperty(PASSIVE_MODE, IsRequired = false)]
+        public BooleanElement PassiveMode
+        {
+            get { return (BooleanElement)this[PASSIVE_MODE]; }
+            set { this[PASSIVE_MODE] = value; }
+        }
 
-		/// <summary>
-		/// Default constructor.
-		/// </summary>
-		public SettingsElement() {
-			DisableMethodBinding = new BooleanElement() ;
-			DisableModelStateBinding = new BooleanElement() ;
-			DisableManager = new BooleanElement() ;
-			ManagerNamespaces = new StringElement() ;
-		}
-	}
+        /// <summary>
+        /// Gets/sets if the generated permalinks should be prefixless.
+        /// </summary>
+        [ConfigurationProperty(PREFIXLESS_PERMALINKS, IsRequired = false)]
+        public BooleanElement PrefixlessPermalinks
+        {
+            get { return (BooleanElement)this[PREFIXLESS_PERMALINKS]; }
+            set { this[PREFIXLESS_PERMALINKS] = value; }
+        }
 
-	/// <summary>
-	/// A configuration element with a boolean value.
-	/// </summary>
-	internal class BooleanElement : ConfigurationElement
-	{
-		/// <summary>
-		/// Gets/sets the element value.
-		/// </summary>
-		[ConfigurationProperty("value", IsRequired=true)]
-		public bool Value { 
-			get { return (bool)this["value"] ; }
-			set { this["value"] = value ; }
-		}
-	}
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public SettingsElement()
+        {
+            DisableMethodBinding = new BooleanElement();
+            DisableModelStateBinding = new BooleanElement();
+            DisableManager = new BooleanElement();
+            ManagerNamespaces = new StringElement();
+        }
+    }
 
-	/// <summary>
-	/// A configuration element with a string value.
-	/// </summary>
-	internal class StringElement : ConfigurationElement
-	{
-		/// <summary>
-		/// Gets/sets the element value.
-		/// </summary>
-		[ConfigurationProperty("value", IsRequired=true)]
-		public string Value {
-			get { return (string)this["value"] ; }
-			set { this["value"] = value ; }
-		}
-	}
+    /// <summary>
+    /// A configuration element with a boolean value.
+    /// </summary>
+    internal class BooleanElement : ConfigurationElement
+    {
+        /// <summary>
+        /// Gets/sets the element value.
+        /// </summary>
+        [ConfigurationProperty("value", IsRequired = true)]
+        public bool Value
+        {
+            get { return (bool)this["value"]; }
+            set { this["value"] = value; }
+        }
+    }
+
+    /// <summary>
+    /// A configuration element with a string value.
+    /// </summary>
+    internal class StringElement : ConfigurationElement
+    {
+        /// <summary>
+        /// Gets/sets the element value.
+        /// </summary>
+        [ConfigurationProperty("value", IsRequired = true)]
+        public string Value
+        {
+            get { return (string)this["value"]; }
+            set { this["value"] = value; }
+        }
+    }
 }
